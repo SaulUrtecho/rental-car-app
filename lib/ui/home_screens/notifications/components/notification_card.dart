2c0016fc6b8601455a3030ca74f906d31db3d682
SaulUrtecho <saul.urtecho93@gmail.com>
2022-11-06 23:11:05 -0600

    upload changes

---
+++ lib/ui/home_screens/notifications/components/notification_card.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:rental_car_app/ui/design/app_colors.dart';
+import 'package:rental_car_app/ui/design/card_shadow.dart';
+
+class NotificationCard extends StatelessWidget {
+  final NotificationData notification;
+
+  const NotificationCard({super.key, required this.notification});
+
+  @override
+  Widget build(BuildContext context) {
+    return CardShadow(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      child: Row(
+        children: [
+          const NotificationIcon(),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                notification.title,
+                style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                notification.description,
+                style: const TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                notification.descrition2,
+                style: const TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                notification.timeStamp,
+                style: const TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NotificationIcon extends StatelessWidget {
+  const NotificationIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: const BoxDecoration(
+            color: AppColors.green,
+            shape: BoxShape.circle,
+          ),
+        ),
+        const Icon(
+          Icons.notifications,
+          color: Colors.white,
+        )
+      ],
+    );
+  }
+}
+
+class NotificationData {
+  final String title;
+  final String description;
+  final String descrition2;
+  final String timeStamp;
+
+  NotificationData({
+    required this.title,
+    required this.description,
+    required this.descrition2,
+    required this.timeStamp,
+  });
+}
--- lib/ui/home_screens/notifications/notifications_screen.dart
+++ lib/ui/home_screens/notifications/notifications_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:rental_car_app/ui/design/app_colors.dart';
-import 'package:rental_car_app/ui/design/card_shadow.dart';
+import 'package:rental_car_app/ui/home_screens/notifications/components/notification_card.dart';
 
-class NotificationsScreen extends StatelessWidget {
+class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({super.key});
+
+  @override
+  State<NotificationsScreen> createState() => _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  final List<NotificationData> notifications = [
+    NotificationData(
+      description: 'Hurry Up! Dont sit at home',
+      descrition2: 'today. Best car rental deal today',
+      timeStamp: '3 min ago',
+      title: '40% off today',
+    ),
+    NotificationData(
+      description: 'Congratulation. Your audi A8 L',
+      descrition2: 'booked successfully.',
+      timeStamp: '3 min ago',
+      title: 'Booking successful',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,75 +39,16 @@ class NotificationsScreen extends StatelessWidget {
         backgroundColor: Colors.white10,
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 8),
-            const Text('Today', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            CardShadow(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-              child: Row(
-                children: [
-                  const NotificationIcon(),
-                  const SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        '40% off today',
-                        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
-                      ),
-                      SizedBox(height: 6),
-                      Text(
-                        '40% off today',
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                      SizedBox(height: 6),
-                      Text(
-                        '40% off today',
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                      SizedBox(height: 6),
-                      Text(
-                        '40% off today',
-                        style: TextStyle(color: Colors.grey, fontSize: 12),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        children: [
+          const Text('Today', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 8),
+          NotificationCard(notification: notifications[0]),
+          const SizedBox(height: 16),
+          NotificationCard(notification: notifications[1]),
+        ],
       ),
-    );
-  }
-}
-
-class NotificationIcon extends StatelessWidget {
-  const NotificationIcon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: AlignmentDirectional.center,
-      children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: const BoxDecoration(
-            color: AppColors.green,
-            shape: BoxShape.circle,
-          ),
-        ),
-        const Icon(
-          Icons.notifications,
-          color: Colors.white,
-        )
-      ],
     );
   }
 }
