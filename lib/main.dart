1f066321ad0d76f9a9df080ea3e553d2cd155299
SaulUrtecho <saul.urtecho93@gmail.com>
2022-12-15 22:07:45 -0600

    bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rental_car_app/ui/app.dart';
+import 'package:rental_car_app/ui/bloc_observer.dart';
 
-void main() => runApp(const App());
+void main() {
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const App());
+}
